adee507ccd2cba6eebf448877e8dc8cfdbe02f98
mujibk7 <mujibk7@gmail.com>
2024-01-12 01:55:28 +0500

    back images added

--- src/api/WeatherApiData.jsx
+++ src/api/WeatherApiData.jsx
@@ -4,6 +4,10 @@ import clouds from '../assets/icons/clouds.png'
 import clear from '../assets/icons/clear.png'
 import rain from '../assets/icons/rain.png'
 import snow from '../assets/icons/snow.png'
+import backClouds from '../assets/images/clouds.webp'
+import backClear from '../assets/images/clear.jpg'
+import backSnow from '../assets/images/snow.webp'
+import backRain from '../assets/images/rain.jpg'
 
 const api = {
   key: "c17e5fb23fca9740b13943342b34aef5",
@@ -15,6 +19,7 @@ function WeatherApiData() {
   const [search, setSearch] = useState('Balakot')
   const [weather, setWeather] = useState("")
   const [city, setCity] = useState("Enter The City Name")
+
 
   const searchPressed = () => {
     fetch(`${api.url}weather?q=${search}&units=metric&APPID=${api.key}`)
@@ -30,18 +35,25 @@ function WeatherApiData() {
 
   return (
     <>
-      <div className="absolute text-center translate-y-24 mx-2 w-full ">
+     <div className="absolute text-center translate-y-24 mx-2 w-full" >
         <div className="py-5">
-          <h3 className="font-serif font-extrabold text-2xl uppercase tracking-widest">{weather.name|| 'Weather App'}</h3>
+          <h3 className="font-serif font-extrabold text-2xl uppercase tracking-widest">{weather.name || 'Weather App'}</h3>
         </div>
 
         {
           typeof weather.main !== "undefined" ? (
-            <div className="bg-orange-400 w-80 mx-auto p-5 rounded-lg shadow-2xl mb-3">
+            <div className="bg-orange-400 w-80 mx-auto p-5 rounded-lg shadow-2xl mb-3" style={{
+              backgroundImage: weather.weather[0]?.main === "Clouds" ? `url(${backClouds})` : 
+              weather.weather[0]?.main === "Clear" ? `url(${backClear})` :
+              weather.weather[0]?.main === "Snow" ? `url(${backSnow})` : 
+              weather.weather[0]?.main === "Rain" ? `url(${backRain})` :
+              ''
+              , backgroundSize:'cover'
+            }}>
               <div className="mb-3">
                 <div className="">
-                  {weather.name}
-                 
+                 <h3 className='`${weather.weather[0].main==="Clouds"} ? text-black : text-white` font-bold'> {weather.name}</h3>
+                 <div className="bg-purple-500 opacity-75 rounded-md p-2">
                   <img className="mx-auto" src={
                     weather.weather[0].main === "Clouds" ? clouds :
                       weather.weather[0].main === "Clear" ? clear :
@@ -49,10 +61,13 @@ function WeatherApiData() {
                           weather.weather[0].main === "Partly Clouds" ? partlyCloud :
                             weather.weather[0].main === "Snow" ? snow : ''
                   } alt="" />
+                
+
+                <h3 className='`${weather.weather[0].main==="Clouds"} ? text-black : text-white ` font-bold'>{weather.main.temp} <sup>o</sup>F</h3>
+                <h4 className='`${weather.weather[0].main==="Clouds"} ? text-black : text-white ` font-bold'>{weather.weather[0].main || 'Status'}</h4>
+                <h5 className='`${weather.weather[0].main==="Clouds"} ? text-black : text-white ` font-bold'>{weather.weather[0].description || 'Status Detail'}</h5>
                 </div>
-                <h3>{weather.main.temp} <sup>o</sup>F</h3>
-                <h4>{weather.weather[0].main || 'Status'}</h4>
-                <h5>{weather.weather[0].description || 'Status Detail'}</h5>
+                </div>
               </div>
             </div>
              
